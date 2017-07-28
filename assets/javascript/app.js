e7be5074fafb91c378eab2093c10c6b7984e266e
Cameron Chapman <cchapman@fanthreesixty.com>
2017-07-27 19:31:22 -0500

    Couple of notes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-	var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive]
-})
+
+	var correctAnswers = ["The Soup Nazi", "Brett Leonard"];
+
+
+	$('#submit').on('click', function() {
+
+
+		console.log('question 1 answer =', $('#question_1 input:radio:checked').attr('value'));
+		console.log('is question 1 correct? ', ($('#question_1 input:radio:checked').attr('value') == correctAnswers[0]));
+
+
+	});
+
+});
 
